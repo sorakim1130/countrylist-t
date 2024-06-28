17b7b35d012f6f4f0410bf9dcd59015617d8959b
sorakim <sora6418@gmail.com>
2024-06-28 09:10:39 +0900

    Feat: reset.css 추가

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import CountryItemList from "./components/CountryItemList/CountryItemList.tsx";
+import '../src/styles/reset.css'
+
+function App() {
+
+  return (
+    <>
+      <CountryItemList/>
+    </>
+  )
+}
+
+export default App
