1b1acef0545eb78804c038584d4c3b5d1f487d9d
sorakim <sora6418@gmail.com>
2024-06-28 09:13:41 +0900

    Feat: add selectedItem component

---
+++ src/components/SelectedItem/SelectedItem.tsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+const SelectedItem = () => {
+
+  return (
+    <Container>
+        <h1>Favorite Countries</h1>
+    </Container>
+  )
+}
+
+const Container = styled.div`
+  
+`;
+
+
+export default SelectedItem
